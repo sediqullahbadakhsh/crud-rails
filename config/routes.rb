b36db47fec9f6b651411388cc477e0f2da90d742
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-14 00:17:36 +0430

    add about route,action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
+  get '/about' => 'pages#about'
+
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
