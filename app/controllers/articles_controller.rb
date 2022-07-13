d4c47e6f50ac47cd293251b70c994bd726b7333f
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-14 02:03:07 +0430

    add and save to database functionality on articles table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,13 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+   
+  end
+  private
 end
