1449557994b78188c153c207a9800a9e78b5bd32
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-14 02:33:43 +0430

    refactor create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,17 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path(@article)
+      flash[:notice] = "article was successfully created"
+    else
+      render 'new'
+  end
+  private
   def article_params
     params.require(:article).permit(:title, :description)
   end
-  def create
-    @article = Article.new(article_params)
-    @article.save
-   
-  end
-  private
 end
