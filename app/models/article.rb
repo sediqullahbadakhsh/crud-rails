e9ea5f7fa5b408e70ec738d6396860c92115942f
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-14 02:40:51 +0430

    create model validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+    validates :title, presense: true, length: {minimum: 4, maximum: 50}
+    validates :description, presense: true, length: {minimum: 10, maximum: 500}
 end
