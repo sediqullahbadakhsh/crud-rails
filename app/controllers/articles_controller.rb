6063e3cf7091e92f7f35dd37abc3a507f7bd0e7e
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-14 20:22:47 +0430

    set validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,23 @@
 class ArticlesController < ApplicationController
+
   def new
     @article = Article.new
+  end
+
+  def edit
   end
 
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to articles_path(@article)
       flash[:notice] = "article was successfully created"
+      redirect_to article_path(@article)
     else
       render 'new'
+    end
+  end
+  def show
+      @article = Article.find(params[:id])
   end
   private
   def article_params
